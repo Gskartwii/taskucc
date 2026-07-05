75157520a25d5307006b1979f891ed405454e0cc
Aleksi Hannula <ahannula4+nixgit@gmail.com>
2026-07-05 12:13:34 +0300

    nix: Upgrade to llvm 23

--- shell.nix
+++ shell.nix
@@ -1,15 +1,20 @@
 let
   pkgs = import (builtins.getFlake "nixpkgs") {};
+  lib = pkgs.lib;
 in
-  pkgs.mkShell {
-    nativeBuildInputs = with pkgs; [
-      clang-tools
-      gdb
-    ];
-    env.KAK_EXTRA_CONFIG = pkgs.writeText "tasku-extra.kak" ''
-      hook global WinSetOption filetype=(c|cpp) %{
-        expandtab
-        set window formatcmd clang-format
-      }
-    '';
-  }
+  # Need clang-format 23 for PackArguments: OnePerLine...
+  assert lib.versionOlder pkgs.llvmPackages_git.clang-tools.version "24";
+  assert lib.versionAtLeast pkgs.llvmPackages_git.clang-tools.version "23";
+  assert lib.versionOlder pkgs.llvmPackages.clang-tools.version "23";
+    pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        llvmPackages_git.clang-tools
+        gdb
+      ];
+      env.KAK_EXTRA_CONFIG = pkgs.writeText "tasku-extra.kak" ''
+        hook global WinSetOption filetype=(c|cpp) %{
+          expandtab
+          set window formatcmd clang-format
+        }
+      '';
+    }
