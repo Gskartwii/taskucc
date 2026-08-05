d93bf4126406bcb8a87729a487d12098357f150b
Aleksi Hannula <ahannula4+nixgit@gmail.com>
2026-08-05 17:18:33 +0300

    nix: Use llvmPackages_23

--- shell.nix
+++ shell.nix
@@ -2,13 +2,9 @@ let
   pkgs = import (builtins.getFlake "nixpkgs") {};
   lib = pkgs.lib;
 in
-  # Need clang-format 23 for PackArguments: OnePerLine...
-  assert lib.versionOlder pkgs.llvmPackages_git.clang-tools.version "24";
-  assert lib.versionAtLeast pkgs.llvmPackages_git.clang-tools.version "23";
-  assert lib.versionOlder pkgs.llvmPackages.clang-tools.version "23";
     pkgs.mkShell {
       nativeBuildInputs = with pkgs; [
-        llvmPackages_git.clang-tools
+        llvmPackages_23.clang-tools
         gdb
       ];
       env.KAK_EXTRA_CONFIG = pkgs.writeText "tasku-extra.kak" ''
