71b013d8b034d22cc5a7d7dc0244a62fc19335c8
Aleksi Hannula <ahannula4+nixgit@gmail.com>
2026-08-15 09:45:11 +0300

    nix: Add llvm binutils to PATH

--- shell.nix
+++ shell.nix
@@ -2,15 +2,16 @@ let
   pkgs = import (builtins.getFlake "nixpkgs") {};
   lib = pkgs.lib;
 in
-    pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [
-        llvmPackages_23.clang-tools
-        gdb
-      ];
-      env.KAK_EXTRA_CONFIG = pkgs.writeText "tasku-extra.kak" ''
-        hook global WinSetOption filetype=(c|cpp) %{
-          expandtab
-          set window formatcmd "clang-format --assume-filename=%val{bufname}"
-        }
-      '';
-    }
+  pkgs.mkShell {
+    nativeBuildInputs = with pkgs; [
+      llvmPackages_23.clang-tools
+      llvmPackages_23.llvm
+      gdb
+    ];
+    env.KAK_EXTRA_CONFIG = pkgs.writeText "tasku-extra.kak" ''
+      hook global WinSetOption filetype=(c|cpp) %{
+        expandtab
+        set window formatcmd "clang-format --assume-filename=%val{bufname}"
+      }
+    '';
+  }
