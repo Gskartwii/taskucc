44e144070c775b01e4c886f84875de95e6aebf4a
Aleksi Hannula <ahannula4+nixgit@gmail.com>
2026-09-06 09:46:06 +0300

    test: Add integer conversion test

--- test/compile/cases/int/driver.c
+++ test/compile/cases/int/driver.c
@@ -1,10 +1,54 @@
+#include "casters.inc"
+#include <limits.h>
 #include <stdio.h>
 
-extern int simple(int);
+extern int konst(void);
+
+#define DECL_CASTER(from, to, n) extern to cast_##n(from);
+#define CHECK_CASTER(from, to, n)                                     \
+    if (cast_##n((from) check_n) != ((to) (from) check_n)) {          \
+        printf("caster failed on " #from " -> " #to ": %lld != %lld", \
+               (long long) cast_##n((from) check_n),                  \
+               (long long) ((to) (from) check_n));                    \
+        ok = 0;                                                       \
+    }
+
+#define CASTER_ACTION DECL_CASTER
+CASTER_LIST
+#undef CASTER_ACTION
+
+#define CASTER_ACTION CHECK_CASTER
 
 int main(void) {
-    int ret_val = simple(0);
+    _Bool ok = 1;
+    long long check_ns[] = {
+        0,
+        1,
+        -1,
+        -50,
+        255,
+        128,
+        -256,
+        65535,
+        -32768,
+        32768,
+        32767,
+        INT_MIN,
+        INT_MAX,
+        LONG_MIN,
+        LONG_MAX,
+        LLONG_MIN,
+        LLONG_MAX,
+    };
 
-    printf("ret_val = %d\n", ret_val);
-    return !(ret_val == 42);
+    if (konst() != 42) {
+        printf("konst() != 42");
+    }
+
+    for (size_t n = 0; n < sizeof(check_ns) / sizeof(check_ns[0]); n++) {
+        long long check_n = check_ns[n];
+        CASTER_LIST
+    }
+
+    return !ok;
 }
--- test/compile/cases/int/test.c
+++ test/compile/cases/int/test.c
@@ -1,3 +1,8 @@
-int simple(int x) {
-    return 42;
-}
+#include "casters.inc"
+#define MK_CASTER(from, to, n)        \
+    to cast_##n(from x) { return x; }
+
+int konst(void) { return 42; }
+
+#define CASTER_ACTION MK_CASTER
+CASTER_LIST
